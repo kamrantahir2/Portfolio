dd0d1cd8873433d4288f1203c0e928c4aca39138
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-06-09 19:15:55 +0100

    Make home page responsive

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,10 +3,10 @@ import { motion } from "framer-motion";
 const Landing = () => {
   return (
     <div className="">
-      <div className="n h-2/12 hero bg-black pt-12">
+      <div className="hero bg-black pt-12 ">
         <div className="landing-container">
           <motion.div
-            className="landing-bg"
+            className="landing-bg w-2/3 h-96 bg-no-repeat"
             animate={{ backgroundSize: "100%" }}
             initial={{ backgroundSize: "200%" }}
             transition={{
@@ -19,17 +19,19 @@ const Landing = () => {
           />
         </div>
 
-        <div className="relative -inset-y-28 inline-block">
-          <h3 className=" text-3xl w-9/12 m-auto text-left">Hey, my name is</h3>
-          <h1 className="text-9xl font-bold bg-gradient-to-r from-orange-500 to-yellow-300 text-transparent bg-clip-text font-permanentMarker">
+        <div className="relative  -inset-y-60 md:-inset-y-28 inline-block">
+          <h3 className=" text-3xl text-wrap w-9/12 m-auto text-left">
+            Hey, my name is
+          </h3>
+          <h1 className="text-6xl md:text-9xl font-bold bg-gradient-to-r from-orange-500 to-yellow-300 text-transparent bg-clip-text font-permanentMarker">
             Kamran Tahir
           </h1>
-          <h2 className=" text-left mt-16 text-4xl tracking-wide w-9/12 m-auto font-medium">
+          <h2 className=" text-left mt-16 text-2xl md:text-4xl tracking-wide w-9/12 m-auto font-medium">
             I build full-stack applications using React & NodeJS and backends
             using Java and Spring Boot.
           </h2>
 
-          <div className="flex ml-48 justify-start mt-12">
+          <div className="md:flex md:ml-48 justify-start mt-12">
             <button className="bg-gradient-to-t from-indigo-800 to-cyan-700 border-none text-gray-50 hover:bg-gradient-to-t hover:from-indigo-900 hover:to-cyan-900 w-36 ">
               My Github
             </button>
@@ -42,10 +44,10 @@ const Landing = () => {
 
       <div className="w-8/12 mt-12 m-auto">
         <h1 className="font-semibold text-6xl">About Me</h1>
-        <div className="w-3/12 h-1 bg-gradient-to-r from-green-500 to-blue-300 mt-4 m-auto"></div>
+        <div className="w-3/12 h-0.5 rounded-full bg-gradient-to-r from-green-500 to-blue-300 mt-4 m-auto"></div>
       </div>
-      <div className="flex container">
-        <h3 className="mt-8 mb-20 text-2xl w-6/12 m-auto leading-relaxed text-left">
+      <div className="grid m-auto lg:grid-cols-2 grid-cols-1 container">
+        <h3 className="mt-8 mb-20 text-2xl lg:ml-28 m-auto leading-relaxed md:text-left col-span-10 lg:col-span-1  lg:w-full w-10/12">
           I am a full-stack developer with a passion for problem solving and
           expanding my knowledge. Originally focusing on building backends using
           Java and Spring Boot, I now also build full-stack applications using
@@ -59,7 +61,7 @@ const Landing = () => {
         </h3>
         <img
           src="./tools-and-utensils.png"
-          className=" w-3/12 -ml-24 mr-32 h-3/4 m-auto  rounded-full"
+          className=" w-4/12 md:w-6/12 m-auto lg:w-8/12 lg:ml-32 rounded-full"
           alt=""
         />
       </div>
@@ -67,15 +69,15 @@ const Landing = () => {
       <div className="mb-40">
         <div className="w-8/12 mt-12 m-auto">
           <h1 className="font-semibold text-6xl">Languages and Tools</h1>
-          <div className="w-5/12 h-1 bg-gradient-to-r from-green-500 to-blue-300 mt-4 m-auto"></div>
+          <div className="w-5/12 h-0.5 rounded-full bg-gradient-to-r from-green-500 to-blue-300 mt-4 m-auto"></div>
         </div>
 
         <div className="showcase container m-auto">
-          <div className="mt-12 grid grid-cols-3">
+          <div className="mt-12 grid lg:grid-cols-3 grid-cols-2">
             <div className="grouped mx-10">
               <h3 className="text-3xl">Languages</h3>
               <div className="divider"></div>
-              <div className="icons gap-4  m-auto grid-cols-2  grid mt-8">
+              <div className="icons gap-4  m-auto lg:grid-cols-3 grid-cols-2  grid mt-8">
                 <div className="icon">
                   <img
                     src="./js.png"
@@ -86,7 +88,7 @@ const Landing = () => {
                 </div>
                 <div className="icon">
                   <img src="./java.png" className="w-14 m-auto" alt="" />
-                  <h2>Java</h2>
+                  <h2 className="">Java</h2>
                 </div>
                 <div className="icon">
                   <img src="./typescript.png" className="w-14 m-auto" alt="" />
@@ -105,7 +107,7 @@ const Landing = () => {
             <div className="grouped mx-10">
               <h3 className="text-3xl ">Libraries & Frameworks</h3>
               <div className="divider"></div>
-              <div className="icons mt-8 gap-4 grid grid-cols-3 ">
+              <div className="icons mt-8 gap-4 grid lg:grid-cols-3 grid-cols-2 ">
                 <div className="icon">
                   <img
                     src="./nodejs.png"
@@ -139,7 +141,7 @@ const Landing = () => {
             <div className="grouped mx-10">
               <h3 className="text-3xl ">Databases</h3>
               <div className="divider"></div>
-              <div className="icons mt-8 grid gap-4 grid-cols-3 ">
+              <div className="icons mt-8 grid lg:grid-cols-3 grid-cols-2 gap-8">
                 <div className="icon">
                   <img
                     src="./postgresql.png"
