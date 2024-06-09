c8c891861861ee22427ff15d850f6ba96f910d27
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-06-09 16:56:03 +0100

    Add languages and framewroks section (still needs to be formatted

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -44,7 +44,7 @@ const Landing = () => {
         <h1 className="font-semibold text-6xl">About Me</h1>
         <div className="w-3/12 h-1 bg-gradient-to-r from-green-500 to-blue-300 mt-4 m-auto"></div>
       </div>
-      <div className="inline-flex">
+      <div className="flex container">
         <h3 className="mt-8 mb-20 text-2xl w-6/12 m-auto leading-relaxed text-left">
           I am a full-stack developer with a passion for problem solving and
           expanding my knowledge. Originally focusing on building backends using
@@ -59,9 +59,104 @@ const Landing = () => {
         </h3>
         <img
           src="./tools-and-utensils.png"
-          className="h-full w-3/12 pb-20 mr-28 m-auto"
+          className=" w-3/12 -ml-24 mr-32 h-3/4 m-auto  rounded-full"
           alt=""
         />
+      </div>
+
+      <div className="mb-40">
+        <div className="w-8/12 mt-12 m-auto">
+          <h1 className="font-semibold text-6xl">Languages and Tools</h1>
+          <div className="w-5/12 h-1 bg-gradient-to-r from-green-500 to-blue-300 mt-4 m-auto"></div>
+        </div>
+
+        <div className="showcase">
+          <div className="mt-12">
+            <div className="grouped">
+              <h3 className="text-3xl">Languages</h3>
+              <div className="icons mt-8">
+                <div className="icon">
+                  <img
+                    src="./js.png"
+                    className="w-14 rounded-xl m-auto"
+                    alt=""
+                  />
+                  <h2>JS</h2>
+                </div>
+                <div className="icon">
+                  <img src="./java.png" className="w-14 m-auto" alt="" />
+                  <h2>Java</h2>
+                </div>
+                <div className="icon">
+                  <img src="./typescript.png" className="w-14 m-auto" alt="" />
+                  <h2>Typescript</h2>
+                </div>
+                <div className="icon">
+                  <img src="./html.png" className="w-14 m-auto" alt="" />
+                  <h2>HTML</h2>
+                </div>
+                <div className="icon">
+                  <img src="./css.png" className="w-14 m-auto" alt="" />
+                  <h2>CSS</h2>
+                </div>
+              </div>
+            </div>
+            <div className="grouped">
+              <h3 className="text-3xl mt-4">Libraries & Frameworks</h3>
+              <div className="icons mt-8">
+                <div className="icon">
+                  <img
+                    src="./nodejs.png"
+                    className="w-14 rounded-xl m-auto"
+                    alt=""
+                  />
+                  <h2>Node.js</h2>
+                </div>
+                <div className="icon">
+                  <img src="./physics.png" className="w-14 m-auto" alt="" />
+                  <h2>React</h2>
+                </div>
+                <div className="icon">
+                  <img src="./express.png" className="w-14 m-auto" alt="" />
+                  <h2>Express</h2>
+                </div>
+                <div className="icon">
+                  <img src="./springboot.png" className="w-14 m-auto" alt="" />
+                  <h2>Spring Boot</h2>
+                </div>
+                <div className="icon">
+                  <img src="./tailwind.png" className="w-14 m-auto" alt="" />
+                  <h2>Tailwind CSS</h2>
+                </div>
+                <div className="icon">
+                  <img src="./bootstrap.png" className="w-14 m-auto" alt="" />
+                  <h2>Bootstrap</h2>
+                </div>
+              </div>
+            </div>
+            <div className="grouped">
+              <h3 className="text-3xl mt-4">Databases</h3>
+              <div className="icons mt-8">
+                <div className="icon">
+                  <img
+                    src="./postgresql.png"
+                    className="w-14 rounded-xl m-auto"
+                    alt=""
+                  />
+                  <h2>PostgreSQL</h2>
+                </div>
+                <div className="icon">
+                  <img src="./mongodb.png" className="w-14 m-auto" alt="" />
+                  <h2>MongoDB</h2>
+                </div>
+                <div className="icon">
+                  <img src="./mysql.png" className="w-14 m-auto" alt="" />
+                  <h2>MySQL</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
