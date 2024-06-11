a653a50489380a012eee6287357b5d1dcbb4ab5e
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-06-11 23:52:54 +0100

    Build Portolio page component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,17 +1,48 @@
 import { projects } from "../data";
+import { motion } from "framer-motion";
 
 const Projects = () => {
   return (
-    <div>
-      <ul>
-        {projects.map((p) => {
+    <div className="mt-20">
+      <h1
+        className="text-6xl md:text-7xl font-bold bg-gradient-to-r from-orange-500 to-yellow-300 
+      
+      text-transparent bg-clip-text font-permanentMarker tracking-widest p-4"
+      >
+        My Projects
+      </h1>
+      <div className="w-6/12 lg:w-3/12 h-1 bg-gradient-to-r from-green-500 to-blue-300 mx-auto my-8"></div>
+
+      <h2 className=" text-center lg:text-left mt-12 lg:mt-32 md:mt-16 text-2xl md:text-3xl tracking-wide w-9/12 m-auto font-medium">
+        Here are the various projects that I have built. This list is
+        ever-growing so check back regularly to see my latest works.
+      </h2>
+
+      <div className="grid grid-cols-1 my-12 lg:grid-cols-2 gap-12">
+        {projects.map((project) => {
           return (
-            <div key={p.image}>
-              <p>{p.title}</p>
+            <div
+              className="card mx-8 lg:mx-24 my-4 lg:my-12 p-4 bg-black shadow-xl "
+              key={project.image}
+            >
+              <figure>
+                <img className="rounded-3xl" src={project.image} alt="Shoes" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{project.title}</h2>
+                <p className="pt-4">{project.description}</p>
+                <a
+                  href={project.link}
+                  target="_blank"
+                  className="card-actions justify-end"
+                >
+                  <button className="btn btn-primary">Learn More</button>
+                </a>
+              </div>
             </div>
           );
         })}
-      </ul>
+      </div>
     </div>
   );
 };
