e21361be534176e938f8d4cf476df1b9f4739ecb
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-06-12 01:26:58 +0100

    Styling change for portfolio ehader

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,25 +3,28 @@ import { Fade, Slide } from "react-awesome-reveal";
 
 const Projects = () => {
   return (
-    <div className="mt-20">
-      <h1
-        className="text-6xl md:text-7xl font-bold bg-gradient-to-r from-orange-500 to-yellow-300 
+    <div className="">
+      <div className="bg-black py-20 lg:py-40 portfolio-header">
+        <Slide triggerOnce direction="left" duration={1000}>
+          <h1
+            className="text-6xl md:text-7xl font-bold bg-gradient-to-r from-orange-500 to-yellow-300 
       
       text-transparent bg-clip-text font-permanentMarker tracking-widest p-4"
-      >
-        My Projects
-      </h1>
-      <div className="w-6/12 lg:w-3/12 h-1 bg-gradient-to-r from-green-500 to-blue-300 mx-auto my-8"></div>
+          >
+            My Projects
+          </h1>
+          <div className="w-6/12 lg:w-3/12 h-1 bg-gradient-to-r from-green-500 to-blue-300 mx-auto my-8"></div>
+        </Slide>
 
-      <h2 className=" text-center lg:text-left mt-12 lg:mt-32 md:mt-16 text-2xl md:text-3xl tracking-wide w-9/12 m-auto font-medium ">
-        Here are the various projects that I have built. This list is
-        ever-growing so check back regularly to see my latest works.
-      </h2>
-
-      <div className=" grid grid-cols-1 my-12 lg:grid-cols-2   gap-12 grid-flow-rows">
+        <h2 className=" text-center lg:text-left mt-12 lg:mt-32 md:mt-16 text-2xl md:text-3xl tracking-wide w-9/12 m-auto font-medium ">
+          Here you will find the various projects that I have built. This list
+          is ever-growing so check back regularly to see my latest works.
+        </h2>
+      </div>
+      <div className=" grid grid-cols-1 mt-32 lg:mt-24 mb-12 lg:grid-cols-2   gap-12 grid-flow-rows">
         {projects.map((project) => {
           return (
-            <Fade duration={1500} cascade key={project.image} triggerOnce>
+            <Fade duration={1000} cascade key={project.image} triggerOnce>
               <div className="card mx-8 lg:mx-24 lg:my-12 p-4 bg-black shadow-xl">
                 <img className="rounded-3xl card-img" src={project.image} />
 
