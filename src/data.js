f119bd2cb0f66b592ff2aa036965e815b552c651
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-06-15 10:24:22 +0100

    Add tags for data

--- src/data.js
+++ src/data.js
@@ -6,6 +6,14 @@ export const projects = [
       "This is my portfolio website built to showcase my various projects. This website was built using React and styled using Tailwind CSS and Daisy UI as well as animations using Framer Motion and React Awesome Reveal.",
     image: "./portfolio.png",
     link: "https://kamran-tahir-portfolio.netlify.app//",
+    tags: [
+      "JavaScript",
+      "React",
+      "React Router",
+      "Tailwind CSS",
+      "Daisy UI",
+      "Framer Motion",
+    ],
   },
   {
     title: "Article Saver",
@@ -14,6 +22,15 @@ export const projects = [
       "Article Saver was created to aid me in my studies by allowing me to keep any articles that I find relating to my studies in one place. This application also allows the user to see articles saved by other users and bookmark them for later use.",
     image: "./allArticles.png",
     link: "https://article-saver-js-fullstack.onrender.com/",
+    tags: [
+      "MERN",
+      "React",
+      "React Router",
+      "MongoDB",
+      "Express",
+      "NodeJS",
+      "Tailwind CSS",
+    ],
   },
 
   {
@@ -23,6 +40,7 @@ export const projects = [
       "Meal Planner is a program that allows the user to plan their meals for the week. The user first inputs their favourite meals and their ingredients then the user inputs what meals they want for each day, after that the program will add the required ingredients to a shopping list. ",
     image: "./addMeals.png",
     link: "https://github.com/kamrantahir2/MealPlanner",
+    tags: ["Java", "Spring Boot", "MySQL"],
   },
   {
     title: "Cashing Up Program",
@@ -31,6 +49,7 @@ export const projects = [
       "This program helps to speed up the till counting process for stores by automating calculations and limiting human error. This application reduced the time taken to count tills by 66%. ",
     image: "./cashingUpProgram.png",
     link: "https://github.com/kamrantahir2/CashingUpProgram_V2",
+    tags: ["Java", "JavaFX", "Scene Builder"],
   },
   {
     title: "Library Management System",
@@ -39,6 +58,7 @@ export const projects = [
       "This API allows the user to add, delete books to a PostgreSQL database as well as allowing the user to rent out and return books",
     image: "./library.jpg",
     link: "https://github.com/kamrantahir2/LibraryManagementRESTService",
+    tags: ["Java", "Spring Boot", "PostgreSQL", "REST"],
   },
   {
     title: "Cinema Management System",
@@ -47,5 +67,6 @@ export const projects = [
       "This API allows the user to book and return seats in a cinema room. This API will also show statistics such as percentage of seats booked and daily earnings. ",
     image: "./cinemaRoom.jpg",
     link: "https://github.com/kamrantahir2/CinemaRESTService",
+    tags: ["Java", "Spring Boot", "PostgreSQL", "REST"],
   },
 ];
