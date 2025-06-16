e321626ff84002c64077fbaba0289a0707374bd5
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2025-06-16 23:55:56 +0100

    Remove sparkles

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -47,7 +47,7 @@ const Landing = () => {
               .
             </h3>
 
-            <div className="md:w-[40rem] w-5/6 h-40 relative mt-10">
+            <div className="w-[40rem] h-40 relative mt-10">
               {/* Gradients */}
               <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
               <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
@@ -55,17 +55,8 @@ const Landing = () => {
               <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
 
               {/* Core component */}
-              <SparklesCore
-                background="transparent"
-                minSize={0.4}
-                maxSize={1}
-                particleDensity={1200}
-                className="w-full mx-auto h-full rounded-full"
-                particleColor="#FFFFFF"
-              />
 
               {/* Radial Gradient to prevent sharp edges */}
-              <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
             </div>
           </div>
         </div>
