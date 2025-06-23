2f7e4f9e1f516e38194092ceb35e9c06c3c8e216
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2025-06-23 20:25:33 +0100

    Change cards design

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,11 +2,35 @@ import { projects } from "../data";
 import { Fade, Slide } from "react-awesome-reveal";
 import { useEffect } from "react";
 import { Highlight } from "./hero-highlight";
+import React, { useId, useRef, useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
+import { useOutsideClick } from "../hooks/use-outside-click";
 
 const Projects = () => {
-  // useEffect(() => {
-  //   window.scrollTo(0, 0);
-  // }, []);
+  const [active, setActive] = useState(null);
+  const id = useId();
+  const ref = useRef(null);
+
+  const cards = projects;
+
+  useEffect(() => {
+    function onKeyDown(event) {
+      if (event.key === "Escape") {
+        setActive(false);
+      }
+    }
+
+    if (active && typeof active === "object") {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    window.addEventListener("keydown", onKeyDown);
+    return () => window.removeEventListener("keydown", onKeyDown);
+  }, [active]);
+
+  useOutsideClick(ref, () => setActive(null));
 
   return (
     <div className="">
@@ -22,59 +46,174 @@ const Projects = () => {
         </h2>
       </div>
 
-      <Fade cascade triggerOnce duration={750}>
-        <div className=" flex flex-row flex-wrap w-full mt-20 lg:mt-24 mb-12 lg:grid-cols-2 ">
-          {projects.map((project) => {
-            return (
-              <div
-                className="card mx-4 lg:mx-24 my-4 lg:my-12 p-4 bg-black shadow-xl py-8 md:w-4/12"
-                key={project.image}
+      <>
+        <AnimatePresence>
+          {active && typeof active === "object" && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="fixed inset-0 bg-black/20 h-full w-full z-10"
+            />
+          )}
+        </AnimatePresence>
+        <AnimatePresence>
+          {active && typeof active === "object" ? (
+            <div className="fixed inset-0  grid place-items-center z-[100]">
+              <motion.button
+                key={`button-${active.title}-${id}`}
+                layout
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: 1,
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: {
+                    duration: 0.05,
+                  },
+                }}
+                className="flex absolute top-2 right-2 lg:hidden items-center justify-center bg-white rounded-full h-6 w-6"
+                onClick={() => setActive(null)}
               >
-                <figure>
-                  <img className="rounded-3xl card-img" src={project.image} />
-                </figure>
+                <CloseIcon />
+              </motion.button>
+              <motion.div
+                layoutId={`card-${active.title}-${id}`}
+                ref={ref}
+                className="w-full max-w-[500px]  h-full md:h-fit md:max-h-[90%]  flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
+              >
+                <motion.div layoutId={`image-${active.title}-${id}`}>
+                  <img
+                    width={200}
+                    height={200}
+                    src={active.image}
+                    alt={active.title}
+                    className="w-full h-80 lg:h-80 sm:rounded-tr-lg sm:rounded-tl-lg object-cover object-top"
+                  />
+                </motion.div>
 
-                <div className="card-body">
-                  <h2 className="card-title  lg:w-9/12 text-2xl">
-                    {project.title}
-                  </h2>
-                  <h2
-                    className="text-left text-lg border-b-2 border-cyan-500 mt-2 pb-5
-                  pl-5"
+                <div>
+                  <div className="flex justify-between items-start p-4">
+                    <div className="">
+                      <motion.h3
+                        layoutId={`title-${active.title}-${id}`}
+                        className="font-medium text-neutral-700 dark:text-neutral-200 text-base"
+                      >
+                        {active.title}
+                      </motion.h3>
+                      <motion.p
+                        layoutId={`description-${active.description}-${id}`}
+                        className="text-neutral-600 dark:text-neutral-400 text-base"
+                      >
+                        {active.description}
+                      </motion.p>
+                    </div>
+
+                    <motion.a
+                      layout
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                      href={active.ctaLink}
+                      target="_blank"
+                      className="px-4 py-3 text-sm rounded-full font-bold bg-green-500 text-white"
+                    >
+                      {active.ctaText}
+                    </motion.a>
+                  </div>
+                  <div className="pt-4 relative px-4">
+                    <motion.div
+                      layout
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                      className="text-neutral-600 text-xs md:text-sm lg:text-base h-40 md:h-fit pb-10 flex flex-col items-start gap-4 overflow-auto dark:text-neutral-400 [mask:linear-gradient(to_bottom,white,white,transparent)] [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch]"
+                    >
+                      {typeof active.content === "function"
+                        ? active.content()
+                        : active.content}
+                    </motion.div>
+                  </div>
+                </div>
+              </motion.div>
+            </div>
+          ) : null}
+        </AnimatePresence>
+        <ul className="max-w-2xl mx-auto w-full grid grid-cols-1 md:grid-cols-2 items-start gap-4">
+          {cards.map((card, index) => (
+            <motion.div
+              layoutId={`card-${card.title}-${id}`}
+              key={card.title}
+              onClick={() => setActive(card)}
+              className="p-4 flex flex-col  hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer"
+            >
+              <div className="flex gap-4 flex-col  w-full">
+                <motion.div layoutId={`image-${card.title}-${id}`}>
+                  <img
+                    width={100}
+                    height={100}
+                    src={card.image}
+                    alt={card.title}
+                    className="h-60 w-full  rounded-lg object-cover object-top"
+                  />
+                </motion.div>
+                <div className="flex justify-center items-center flex-col">
+                  <motion.h3
+                    layoutId={`title-${card.title}-${id}`}
+                    className="font-medium text-neutral-800 dark:text-neutral-200 text-center md:text-left text-base"
                   >
-                    - {project.subtitle}
-                  </h2>
-                  <p className="lg:p-4 md:mb-12 text-lg lg:h-52 leading-8">
-                    {project.description}
-                  </p>
+                    {card.title}
+                  </motion.h3>
+                  <motion.p
+                    layoutId={`description-${card.description}-${id}`}
+                    className="text-neutral-600 dark:text-neutral-400 text-center md:text-left text-base"
+                  >
+                    {card.description}
+                  </motion.p>
                 </div>
-                <div className="grid grid-cols-3 lg:grid-cols-4 gap-4 mb-4 mx-3 mt-8">
-                  {project.tags.map((tag) => {
-                    return (
-                      <div key={tag} className="">
-                        <p className=" rounded-full lg:bg-blue-500 lg:p-1 text-sm text-cyan-500  lg:text-black font-semibold">
-                          {tag}
-                        </p>
-                      </div>
-                    );
-                  })}
-                </div>
-                <a
-                  href={project.link}
-                  target="_blank"
-                  className="card-actions justify-end"
-                >
-                  <button className="btn m-auto lg:m-0 bg-cyan-300 text-black font-bold hover:bg-cyan-200">
-                    Learn More
-                  </button>
-                </a>
               </div>
-            );
-          })}
-        </div>
-      </Fade>
+            </motion.div>
+          ))}
+        </ul>
+      </>
     </div>
   );
 };
 
 export default Projects;
+
+export const CloseIcon = () => {
+  return (
+    <motion.svg
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        opacity: 1,
+      }}
+      exit={{
+        opacity: 0,
+        transition: {
+          duration: 0.05,
+        },
+      }}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="h-4 w-4 text-black"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M18 6l-12 12" />
+      <path d="M6 6l12 12" />
+    </motion.svg>
+  );
+};
